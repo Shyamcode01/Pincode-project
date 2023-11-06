09bd823fa3ecb024a6a9ea4e99cc8e28480bbfd5
shyamcode01 <shyamyadav121240@gmail.com>
2023-11-06 07:40:11 -0800

    change navbar

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -6,7 +6,7 @@ let Navbar=()=>{
         <>
         <nav className="navbar navbar-expand-lg bg-primary ">
   <div className="container-fluid ">
-    <a className="navbar-brand fs-5" href="#">Shya<span className="logol text-danger">m</span></a>
+    <a className="navbar-brand fs-5" href="#">Zip<span className="logol text-danger">Code</span></a>
     
   </div>
 </nav>
